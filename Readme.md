941427eb13fdcd7db10269415b058a11cd47cba6
Angel Ramirez <angel.ramirez@unmsm.edu.pe>
2023-03-15 23:40:04 -0500

    GCLOUD APP funcionando

--- Readme.md
+++ Readme.md
@@ -1,7 +1,13 @@
-#API DE CONSULTA A BASE DE DATOS SQL EN GOOGLE CLOUD
+# API DE CONSULTA A BASE DE DATOS SQL EN GOOGLE CLOUD
 --------
+Archivos que se subieron a google cloud para generar una API de consulta GUEST.
+
 
 Requisitos:
 
 
 
+vm app.yaml
+pip install -r requirements.txt
+
+gcloud app deploy
--- main.py
+++ main.py
@@ -1,45 +1,90 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 import pymysql.cursors
 import os
+from dotenv import load_dotenv
+import json
 
+
+load_dotenv()
 
 app = Flask(__name__)
 
+config = {
+    'user': os.getenv('db_user'),
+    'password': os.getenv('db_password'),
+    'host': os.getenv('cloud_sql_host'),
+    'database': os.getenv('db_name'),
+    'port': os.getenv('db_port'),
+}
 
-# Conexión a la base de datos
-connection = pymysql.Connect(
-    host=os.environ.get('MYSQL_HOST'),
-    user=os.environ.get('MYSQL_USER'),
-    password=os.environ.get('MYSQL_PASSWORD'),
-    db=os.environ.get('MYSQL_DATABASE'),
-    port=os.environ.get('MYSQL_PORT'),
-)
+db_connection_name=os.getenv('db_connection_name'),
 
-# Ruta para la API
-@app.route('/api/query')
-def query():
-    # Obtener el valor del parámetro "id" de la consulta
-    id = request.args.get('id')
+# Ruta para la API en caso no se administre id
+@app.route('/api/', methods=['GET'])
+def main():
+    return json.dumps({'error': 'ID NOT SUPPLIED'}),404
 
-    # Si el parámetro "id" no está presente, devolver un error
-    if id is None:
-        return jsonify({'error': 'No se proporcionó un valor para el parámetro "id".'})
 
-    # Crear un cursor para la base de datos
-    with connection.cursor() as cursor:
-        # Ejecutar la consulta SQL
-        cursor.execute('SELECT * FROM Lista_de_invitados WHERE ID = %s', id)
+@app.route('/api/<id>', methods=['GET'])
+def get_invitado(id):
+    
+    unix_socket = '/cloudsql/{}'.format(db_connection_name)
+    
+    if os.environ.get('GAE_ENV') == 'standard':
+        # Configuración para conexión mediante IP interna de gcloud
+        #Cambiar host por la IP interna de la instancia de Cloud SQL
+        connection = pymysql.Connect(
+                            #Cambiar el host por la ip privada de la instancia de Cloud SQL
+                            host="10.9.16.3",
+                            user=config['user'],
+                            password=config['password'],
+                            db=config['database'],
+                            port=int(config['port']),
+                        )
+    else:
+        connection = pymysql.Connect(
+                            host=config['host'],
+                            user=config['user'],
+                            password=config['password'],
+                            db=config['database'],
+                            port=int(config['port']),
+                        )
         
-        # Obtener los resultados de la consulta
-        result = cursor.fetchone()
+        
+    try:
+    
+        with connection.cursor() as cursor:
 
-        # Si no se encontraron resultados, devolver un error
-        if result is None:
-            return jsonify({'error': 'No se encontró un registro con el ID proporcionado.'})
+            # Si el parámetro "id" no está presente, devolver un error
+            if id is None:
+                return json.dumps({'error': 'ID NOT SUPPLIED'}),404
+            
+            if id == "":
+                return json.dumps({'error': 'ID NOT SUPPLIED'}),404
 
-        # Devolver el resultado como JSON
-        return jsonify(result)
+            # Crear un cursor para la base de datos
+            with connection.cursor() as cursor:
+                # Ejecutar la consulta SQL
+                cursor.execute('SELECT * FROM Lista_de_invitados WHERE ID = %s', id)
+                
+                # Obtener los resultados de la consulta
+                result= cursor.fetchone()
+                
+
+                # Si no se encontraron resultados, devolver un error
+                if result is None:
+                    return jsonify({'error': 'ID NOT FOUND'}),404
+
+                # Devolver el resultado como JSON
+                cursor.execute('SELECT * FROM Lista_de_invitados WHERE ID = %s', id)
+                data=cursor.fetchall()
+                result = {"ID": data[0][0], "SEX": data[0][1], "APELLIDOS": data[0][2], "NOMBRES": data[0][3], "NUMBER_GUEST": data[0][4], "MESA": data[0][5], "CELULAR": data[0][6], "CONFIRMADO": data[0][7]}
+                j_result = json.dumps(result)
+                return j_result,200
+    finally:
+        connection.close()
+
 
 # Iniciar la aplicación
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
